fc3adf29888cb715bdf511f9e64b7bf753897e21
saugatdhakal <saugatdhakal5@gmail.com>
2025-03-03 10:17:39 +0545

    Add Vercel Analytics integration and update package dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import "./index.css";
 import Pomodoro from "./Pomodoro.jsx";
 document.title = `Pomodoro Timer`;
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react"
 import { ThemeProvider } from "./ThemeContext.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider>
       <Pomodoro />
       <SpeedInsights />
+      <Analytics />
     </ThemeProvider>
   </StrictMode>
 );
