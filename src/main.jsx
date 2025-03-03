432eeff60ee686d5f467a6428af0f10258a78f7b
saugatdhakal <saugatdhakal5@gmail.com>
2025-03-03 10:07:51 +0545

    Add Vercel Speed Insights integration and update dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Pomodoro from "./Pomodoro.jsx";
 document.title = `Pomodoro Timer`;
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { ThemeProvider } from "./ThemeContext.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider>
       <Pomodoro />
+      <SpeedInsights />
     </ThemeProvider>
   </StrictMode>
 );
